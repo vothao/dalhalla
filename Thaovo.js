e7c3ca5f4fc6fc3e4d3eb19471aab13ec0bcf28e
vothao <tn.thao.vo@gmail.com>
2019-05-31 11:25:10 +0200

    Add files via upload

---
+++ Thaovo.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+            $('#fullpage').fullpage({
+              sectionsColor: ['#91c5c1', '#91c5c1', '#91c5c1', '#F9C8E1', '#FBDCEB', '#FCEBF4', '#91c5c1'],
+              anchors: ['HejDalhalla', 'OmThao', 'Kommunikator', 'KlubbRepublik', 'Foxen', 'SSKFestival', 'Kontakt'],
+              menu: '#menu',
+              navigation: true,
+
+        navigationPosition: 'right',
+        navigationTooltips: ['Hej Dalhalla', 'Om Thao', 'Som digital kommunikatör', 'Projekt Klubb Republik', 'Projekt Foxen', 'Projekt SSK Festival', 'Kontakt']
+           
+
+            }); });
+    
